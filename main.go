2c97c5dad4f79a7599049b81ae435919e4f784e6
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2021-08-19 15:17:27 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/diamondburned/arikawa/bot"
+)
+
+// To run, do `BOT_TOKEN="TOKEN HERE" go run .`
+
+func main() {
+	var token = os.Getenv("BOT_TOKEN")
+	if token == "" {
+		log.Fatalln("No $BOT_TOKEN given.")
+	}
+
+	commands := &Bot{}
+
+	wait, err := bot.Start(token, commands, func(ctx *bot.Context) error {
+		ctx.HasPrefix = bot.NewPrefix("!", "~")
+		ctx.EditableCommands = true
+
+		// // Subcommand demo, but this can be in another package.
+		// ctx.MustRegisterSubcommand(&Debug{})
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Bot started")
+
+	// As of this commit, wait() will block until SIGINT or fatal. The past
+	// versions close on call, but this one will block.
+	// If for some reason you want the Cancel() function, manually make a new
+	// context.
+	if err := wait(); err != nil {
+		log.Fatalln("Gateway fatal error:", err)
+	}
+}
---
+++ member_api.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/diamondburned/arikawa/discord"
+
+// getDiscordAuthLink returns the Discord authentication link
+// from the authentication server.
+// TODO implement
+func getDiscordAuthLink(user discord.User) string {
+	return "AUTH_LINK_GOES_HERE"
+}
+
+// isDiscordAuthenticated checks whether a user is authenticated
+// in the database for Discord, and returns true if they are, or
+// false otherwise.
+// TODO implement
+func isDiscordAuthenticated(user discord.User) bool {
+	return true
+}
